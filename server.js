5046737841f709e0eb0fd47632e78b701a0583af
DenkyOgbonnaya <denkyogb@gmail.com>
2019-11-02 02:19:56 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 8080
+
+app.use(express.json());
+app.options('*', cors());
+app.use(cors({credentials:true, origin: 'http://localhost:4200'}))
+
+app.get('/', (req, res) => {
+    res.status(200).send('hurray! app is working!.')
+})
+
+
+app.listen(PORT, (err) =>{
+    if(err) throw err ;
+        console.log(`app listening on port ${PORT}`);
+} );
