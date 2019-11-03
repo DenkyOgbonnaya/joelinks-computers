28885f394135e5d90d1c6d15301a8fd43c37812a
DenkyOgbonnaya <denkyogb@gmail.com>
2019-11-02 21:02:12 -0700

    Make changes to enable typescript

--- server.ts
+++ server.ts
@@ -8,12 +8,12 @@ app.use(express.json());
 app.options('*', cors());
 app.use(cors({credentials:true, origin: 'http://localhost:4200'}))
 
-app.get('/', (req, res) => {
-    res.status(200).send('hurray! app is working!.')
+app.get('/', (req:any, res:any) => {
+    res.status(200).send('hurray! app is working!.');
 })
 
 
-app.listen(PORT, (err) =>{
+app.listen(PORT, (err:any) =>{
     if(err) throw err ;
         console.log(`app listening on port ${PORT}`);
 } );
