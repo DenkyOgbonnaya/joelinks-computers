b216a759197fa00ef21c5871dfe576a903bc830e
DenkyOgbonnaya <denkyogb@gmail.com>
2019-11-22 16:41:36 -0800

    Add menu dropdown

---
+++ client/src/app/header/header-dropdown/header-dropdown.component.ts
@@ -0,0 +1,9 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'header-dropdown',
+  templateUrl: './header-dropdown.component.html'
+})
+export class HeaderDropdownComponent {
+    @Input()username:string;
+}
--- client/src/app/header/header.module.ts
+++ client/src/app/header/header.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from "@angular/common";
 import { HeaderComponent } from './header.component';
 import { SubHeader } from './sub-header/subheader.component';
 import { RouterModule } from '@angular/router';
+import { HeaderDropdownComponent } from './header-dropdown/header-dropdown.component';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
     imports: [
         CommonModule,
-        RouterModule
+        RouterModule,
+        NgbModule
     ],
     declarations: [
         HeaderComponent,
-        SubHeader
+        SubHeader,
+        HeaderDropdownComponent
     ],
     exports: [
         HeaderComponent,
