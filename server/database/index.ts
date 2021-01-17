75782a995e25a805fb2d07fba79e0d80b3a2e9be
DenkyOgbonnaya <denkyogb@gmail.com>
2021-01-17 12:27:39 +0100

    Move to mongo atlas

--- server/database/index.ts
+++ server/database/index.ts
@@ -2,13 +2,19 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
-const MONGODB_URL:any = process.env.MONGODB_URL
-const dbUrl = process.env.MONGODB_URI || MONGODB_URL //'mongodb://localhost:27017/joelinks';
+//const MONGODB_URL:any = process.env.MONGODB_URL
+const { DB, DB_USER, DB_PASS } = process.env;
+const dbUrl = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.p1shx.mongodb.net/`; //'mongodb://localhost:27017/joelinks';
 
-const connectToDb = ():void => {
-    mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('connected to joelinks db'))
-    .catch(err => console.log(err))
-}
+const connectToDb = (): void => {
+  mongoose
+    .connect(dbUrl, {
+      dbName: DB,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("connected to joelinks db"))
+    .catch((err) => console.log(err));
+};
 
 export default connectToDb;
